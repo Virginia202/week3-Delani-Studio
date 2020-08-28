1e4408b02388c9e8ec35dbc9f1520726f0e5dece
Virginia202 <virgisonie20@gmail.com>
2020-08-28 23:45:30 +0300

    add section three js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// The toggle effect----Users-interface
+$(document).ready(function(){
+    $("#image1").click(function(){
+        $("#image1").slideDown("fast").hide("1000");
+        $(".our").show("fast")
+    });
+    $(".our").click(function(){
+        $(".our").slideUp("fast");
+        $("#image1").slideDown("fast")
+    });
+
+    $("#image2").click(function(){
+        $("#image2").slideDown("fast").hide("1000");
+        $(".all").show("fast")
+    });
+    $(".all").click(function(){
+        $(".all").slideUp("fast");
+        $("#image2").slideDown("fast")
+    });
+
+    $("#image3").click(function(){
+        $("#image3").slideDown("fast").hide("1000");
+        $(".planning").show("fast")
+    });
+    $(".planning").click(function(){
+        $(".planning").slideUp("fast");
+        $("#image3").slideDown("fast")
+    });
+});
